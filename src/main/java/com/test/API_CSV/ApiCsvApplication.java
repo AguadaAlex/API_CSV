1c3fffeb0ce63e3153b41650ddbfe7f8623cda68
aaguada <alex.aguada@snoopconsulting.com>
2025-03-07 16:51:03 -0300

    mi primer commit del proyecto

---
+++ src/main/java/com/test/API_CSV/ApiCsvApplication.java
@@ -0,0 +1,13 @@
+package com.test.API_CSV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/API_CSV/ApiCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.API_CSV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
